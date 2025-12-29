b0b9d4e3ee19a889fcc3c1e0691fbeb4e966eba7
MUSALEMANOJRAO508 <154533036+MUSALEMANOJRAO508@users.noreply.github.com>
2025-12-29 11:13:58 +0530

    deploying time

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,20 @@
-﻿const express = require('express');
+const express = require('express');
+const cors = require('cors');
+
 const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// test route
 app.get('/', (req, res) => {
   res.send('Backend running');
 });
+
+// port (Render uses process.env.PORT)
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(Server running on ));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
