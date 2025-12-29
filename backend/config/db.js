034e246d93e5f8b0668a0448e88ca82b827c5129
MUSALEMANOJRAO508 <154533036+MUSALEMANOJRAO508@users.noreply.github.com>
2025-12-29 22:39:52 +0530

    Update MongoDB connection string to include database

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () =>{
     try{
-        await mongoose.connect("mongodb+srv://musalemanojraodcme033:Musale@23005A0508@cluster0.yxdk6nk.mongodb.net/?appName=Cluster0");
+        await mongoose.connect("mongodb+srv://musalemanojraodcme033:Musale%4023005A0508@cluster0.yxdk6nk.mongodb.net/collegeDB?appName=Cluster0
+");
         console.log("Mongidb connected successfully");
     }catch(error){
         console.log("mongodb error",error);
